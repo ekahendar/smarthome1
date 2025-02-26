65a348d58f44030925542515a10d227d46702316
ekahendar <hendar1994@gmail.com>
2025-02-26 08:28:37 +0700

    pendalaman routing dengan nuxt-link

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -2,6 +2,13 @@
   <div>
     <h2 class="text-2xl font-bold mb-4">Dashboard</h2>
     <p class="text-gray-700">Selamat datang di Admin Dashboard.</p>
+    <nuxt-link to="/admin/users" class="text-blue-500 hover:underline">
+      Go to Users Dashboard
+    </nuxt-link>
+    <br>
+    <nuxt-link to="/" class="text-blue-500 hover:underline">
+      Go to Root
+    </nuxt-link>
   </div>
 </template>
 
--- pages/admin/users.vue
+++ pages/admin/users.vue
@@ -2,6 +2,9 @@
   <div>
     <h2 class="text-2xl font-bold mb-4">Users</h2>
     <p class="text-gray-700">Manajemen pengguna.</p>
+    <nuxt-link to="/admin" class="text-blue-500 hover:underline">
+        Go to Admin Dashboard
+      </nuxt-link>
     <img src="/images/grid-image/image-01.png" alt="">
   </div>
 </template>
