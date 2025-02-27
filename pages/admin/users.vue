1e492bbc7140a22924cf5b51bbbbd49a3984100b
ekahendar <hendar1994@gmail.com>
2025-02-27 09:32:06 +0700

    input masuk dalam data

--- pages/admin/users.vue
+++ pages/admin/users.vue
@@ -3,8 +3,23 @@
     <h2 class="text-2xl font-bold mb-4">Users</h2>
     <p class="text-gray-700">Manajemen pengguna.</p>
     <nuxt-link to="/admin" class="text-blue-500 hover:underline">
-        Go to Admin Dashboard
-      </nuxt-link>
+      Go to Admin Dashboard
+    </nuxt-link>
+    <input 
+      type="text" 
+      v-model="message" 
+      placeholder="Type something..."
+      class="input-field"
+    >
+
+    <p class="display-text">
+      Your input: <span v-text="message"></span>
+    </p>
+
+    <p class="display-text">
+      Your input (interpolation): {{ message }}
+    </p>
+    
     <img src="/images/grid-image/image-01.png" style="width:200px;height:200px;">
   </div>
 </template>
@@ -12,5 +27,10 @@
 <script>
 export default {
   layout: 'admin', // Gunakan layout admin
+  data() {
+    return {
+      message: ''
+    }
+  }
 };
 </script>
